40ce6326738d0ce8639b489c262d153648149b24
MCT32 <ivanhen99@gmail.com>
2024-12-04 23:19:40 +1100

    login structure

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,13 @@
+<div class="flex items-center justify-center bg-gradient-to-br from-zinc-700 to-zinc-800 w-full min-h-svh">
+    <form class="flex flex-col h-fit bg-zinc-700 p-8 border-zinc-600 rounded-lg border" method="post">
+        <label>
+            Username
+            <input name="username" type="text">
+        </label>
+        <label>
+            Password
+            <input name="password" type="password">
+        </label>
+        <button>Log In</button>
+    </form>
+</div>
