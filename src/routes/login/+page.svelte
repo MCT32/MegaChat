8cf898758fc865a7103bdef38d870c93146851a9
MCT32 <ivanhen99@gmail.com>
2024-12-05 13:19:37 +1100

    login page done

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,13 +1,16 @@
 <div class="flex items-center justify-center bg-gradient-to-br from-zinc-700 to-zinc-800 w-full min-h-svh">
-    <form class="flex flex-col h-fit bg-zinc-700 p-8 border-zinc-600 rounded-lg border" method="post">
-        <label>
+    <form class="flex flex-col h-fit bg-zinc-700 p-8 border-zinc-600 rounded-lg border gap-3" method="post">
+        <h1 class="text-white text-center font-bold text-xl">Login</h1>
+        <label class="text-white">
             Username
-            <input name="username" type="text">
+            <br>
+            <input name="username" type="text" class="bg-transparent border-zinc-600 rounded-md border w-64">
         </label>
-        <label>
+        <label class="text-white">
             Password
-            <input name="password" type="password">
+            <br>
+            <input name="password" type="password" class="bg-transparent border-zinc-600 rounded-md border w-64">
         </label>
-        <button>Log In</button>
+        <button class="text-white bg-green-500 w-fit self-center px-2 py-1 rounded-md shadow-lg">Log In</button>
     </form>
 </div>
