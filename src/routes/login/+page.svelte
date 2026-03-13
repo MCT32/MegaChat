59c7b40eda9364bf74e76095c1b1044ad76fd6e8
Seth Henderson <32090502+MCT32@users.noreply.github.com>
2026-03-13 15:55:48 +1100

    Added API endpoint for login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,16 +1,31 @@
+<script lang="ts">
+    let username = $state('');
+    let password = $state('');
+
+    async function login() {
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+    }
+</script>
+
 <div class="flex items-center justify-center bg-gradient-to-br from-zinc-700 to-zinc-800 w-full min-h-svh">
-    <form class="flex flex-col h-fit bg-zinc-700 p-8 border-zinc-600 rounded-lg border gap-3" method="post">
+    <div class="flex flex-col h-fit bg-zinc-700 p-8 border-zinc-600 rounded-lg border gap-3">
         <h1 class="text-white text-center font-bold text-xl">Login</h1>
         <label class="text-white">
             Username
             <br>
-            <input name="username" type="text" class="bg-transparent border-zinc-600 rounded-md border w-64">
+            <input bind:value={username} type="text" class="bg-transparent border-zinc-600 rounded-md border w-64">
         </label>
         <label class="text-white">
             Password
             <br>
-            <input name="password" type="password" class="bg-transparent border-zinc-600 rounded-md border w-64">
+            <input bind:value={password} type="password" class="bg-transparent border-zinc-600 rounded-md border w-64">
         </label>
-        <button class="text-white bg-green-500 w-fit self-center px-2 py-1 rounded-md shadow-lg">Log In</button>
-    </form>
+        <button onclick={login} class="text-white bg-green-500 w-fit self-center px-2 py-1 rounded-md shadow-lg">Log In</button>
+    </div>
 </div>
